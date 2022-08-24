c75afac6ebd6a69aec1cef6ea450c36af63fcf64
Jade <y3m45566@gmail.com>
2022-08-24 16:37:12 +0800

    save to object, and edit, save, collect function done.

--- Jade/JS/add-new-task.js
+++ Jade/JS/add-new-task.js
@@ -1,37 +1,163 @@
-const taskAddButton = document.querySelector(".task-input__button");
+const taskAdd = document.querySelector(".task-add");
+const taskList = document.querySelector(".task-list");
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-taskAddButton.addEventListener("click", addTaskHandler);
+// 一進來先抓local的東西刷新
+addTaskList(tasks, taskList);
 
-function addTaskHandler() {
-  const taskInput = document.querySelector(".task-input input[type=text]");
+taskAdd.addEventListener("submit", addTaskTable);
+taskList.addEventListener("click", saveTask);
+taskList.addEventListener("click", editTask);
+taskList.addEventListener("click", checkedTask);
+taskList.addEventListener("click", collectTask);
 
-  const newTask = document.createElement("form");
-  newTask.classList.add("task");
-  newTask.innerHTML = `<div class="task__head task__head_border">
+function addTaskTable(e) {
+  e.preventDefault();
+  const title = this.querySelector("input[type=text]").value;
+  const taskItem = {
+    title,
+    date: "",
+    datetime: "",
+    comment: "",
+    file: "",
+    collect: false,
+    folded: false,
+    done: false,
+  };
+
+  tasks.push(taskItem);
+  addTaskList(tasks, taskList);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  this.reset();
+}
+
+function saveTask(e) {
+  if (!e.target.classList.contains("save-button")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  // 選取現在的值
+  const taskTitle = this.querySelector(
+    `.task[data-index="${index}"] input[type="name"]`
+  );
+  const taskComment = this.querySelector(
+    `.task__body[data-index="${index}"] textarea[name="text"]`
+  );
+  const taskDate = this.querySelector(
+    `.task__body[data-index="${index}"] input[type="date"]`
+  );
+  const taskDatetime = this.querySelector(
+    `.task__body[data-index="${index}"] input[type="datetime"]`
+  );
+  tasks[index].comment = taskComment.value;
+  tasks[index].title = taskTitle.value;
+  tasks[index].date = taskDate.value;
+  tasks[index].datetime = taskDatetime.value;
+
+  // 控制收合
+  tasks[index].folded = !tasks[index].folded;
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  addTaskList(tasks, taskList);
+}
+
+function editTask(e) {
+  // || edit icon沒用
+  if (!e.target.classList.contains("edit-button")) return;
+
+  const el = e.target;
+  const index = el.dataset.index;
+
+  tasks[index].folded = !tasks[index].folded;
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  addTaskList(tasks, taskList);
+}
+
+// 案的那個target一定要有data-index=.. 不然會抓不到index，所以等於所有要案的案件都要加上data-index=..
+function checkedTask(e) {
+  if (!e.target.classList.contains("task__checked")) return;
+
+  const el = e.target;
+  const index = el.dataset.index;
+  tasks[index].done = !tasks[index].done;
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  addTaskList(tasks, taskList);
+}
+
+function collectTask(e) {
+  console.log(e.target);
+  if (!e.target.classList.contains("collect-button")) return;
+
+  const el = e.target;
+  const index = el.dataset.index;
+  tasks[index].collect = !tasks[index].collect;
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  addTaskList(tasks, taskList);
+}
+
+// function addFolded(e) {
+//   if (!e.target.classList.contains("save-button")) return;
+//   const el = e.target;
+//   const index = el.dataset.index;
+//   tasks[index].folded = !tasks[index].folded;
+
+//   const taskTitle = document.querySelector(
+//     `.task__title[data-index="${index}"]`
+//   );
+//   const taskHead = document.querySelector(`.task__head[data-index="${index}"]`);
+//   const taskBody = document.querySelector(`.task__body[data-index="${index}"]`);
+//   const taskStatus = document.querySelector(
+//     `.task__status[data-index="${index}"]`
+//   );
+
+//   taskTitle.setAttribute("disabled", "disabled");
+//   taskHead.classList.toggle("task__head_border");
+//   taskBody.classList.toggle("folded");
+//   taskStatus.classList.toggle("folded");
+
+//   localStorage.setItem("tasks", JSON.stringify(tasks));
+//   addTaskList(tasks, taskList);
+// }
+
+function addTaskList(tasks = [], taskList) {
+  taskList.innerHTML = tasks
+    .map((task, index) => {
+      return `<div class="task ${
+        task.collect ? "collect-mode" : ""
+      }" data-index="${index}" >
+    <div class="task__head ${
+      task.folded ? "" : "task__head_border"
+    }" data-index="${index}">
   <div class="task__head__main">
-    <input type="checkbox" name="checkbox"/>
+    <input class="task__checked" type="checkbox" name="checkbox" data-index="${index}" ${
+        task.done ? "checked" : ""
+      }/>
     <input
-      type="name"
+      type="name" d
       class="task__title"
       placeholder="type something here..."
-      value="${taskInput.value}"
-    />
+      value="${task.title}"
+      ${task.folded ? 'disabled="disabled"' : ""} />
   </div>
-  <div class="task__head__icon">
-    <button type="button" class="collect-button">
-      <i class="fa-regular fa-star"></i>
+  <div class="task__head__icon" data-index="${index}">
+    <button type="button" class="collect-button" data-index="${index}">
+      <i class="${
+        task.collect ? "fa-solid collect-color" : "fa-regular"
+      } fa-star"></i>
     </button>
-    <button type="button" class="edit-button">
-      <i class="fa-solid fa-pen" > </i >
+    <button type="button" class="edit-button" data-index="${index}">
+      <i class="fa-solid fa-pen"></i>
     </button>
   </div>
 </div>
-<div class="task__body">
+<div class="task__body ${task.folded ? "folded" : ""}" data-index="${index}">
   <div class="date">
     <h3>dateline</h3>
     <div class="date__input">
-      <input type="text" placeholder="yyyy/mm/dd" />
-      <input type="text" placeholder="hh:mm" />
+      <input type="date" placeholder="yyyy/mm/dd" value="${task.date}"/>
+      <input type="datetime" placeholder="hh:mm" value="${task.datetime}"/>
     </div>
   </div>
   <div class="file">
@@ -42,24 +168,89 @@ function addTaskHandler() {
   <div class="comment">
     <h3>comment</h3>
     <textarea
-      name=""
-      id="comment"
+      name="text"
       cols="10"
       rows="4"
       placeholder="type your memo here..."
-    ></textarea>
+    >${task.comment === "" ? "" : task.comment}</textarea>
   </div>
 </div>
-<div class="task__status">
-  <button class="delete-button" type="button">
+<div class="task__status ${task.folded ? "folded" : ""}" data-index="${index}">
+  <button class="delete-button" type="button" data-index="${index}">
     <i class="fa-regular fa-x"></i>
     <p>delete</p>
   </button>
-  <button class="save-button" type="button">
+  <button class="save-button" type="button" data-index="${index}">
     <i class="fa-regular fa-plus"></i>
     <p>save</p>
   </button>
+</div>
 </div>`;
-  // newTask.childNodes[0].childNodes[1].childNodes[3].value = taskInput.value;
-  list.appendChild(newTask);
+    })
+    .join("");
 }
+
+// 事件驅動
+// function addTaskHandler() {
+//   const taskInput = document.querySelector(".task-add input[type=text]");
+
+//   const newTask = document.createElement("form");
+//   newTask.classList.add("task");
+//   newTask.innerHTML = `<div class="task__head task__head_border">
+//   <div class="task__head__main">
+//     <input type="checkbox" name="checkbox"/>
+//     <input
+//       type="name"
+//       class="task__title"
+//       placeholder="type something here..."
+//       value="${taskInput.value}"
+//     />
+//   </div>
+//   <div class="task__head__icon">
+//     <button type="button" class="collect-button">
+//       <i class="fa-regular fa-star"></i>
+//     </button>
+//     <button type="button" class="edit-button">
+//       <i class="fa-solid fa-pen"></i>
+//     </button>
+//   </div>
+// </div>
+// <div class="task__body">
+//   <div class="date">
+//     <h3>dateline</h3>
+//     <div class="date__input">
+//       <input type="text" placeholder="yyyy/mm/dd" />
+//       <input type="text" placeholder="hh:mm" />
+//     </div>
+//   </div>
+//   <div class="file">
+//     <h3>File</h3>
+//     <label for="file"><i class="fa-solid fa-square-plus"></i></label>
+//     <input id="file" type="file" name="" accept="" />
+//   </div>
+//   <div class="comment">
+//     <h3>comment</h3>
+//     <textarea
+//       name=""
+//       id="comment"
+//       cols="10"
+//       rows="4"
+//       placeholder="type your memo here..."
+//     ></textarea>
+//   </div>
+// </div>
+// <div class="task__status">
+//   <button class="delete-button" type="button">
+//     <i class="fa-regular fa-x"></i>
+//     <p>delete</p>
+//   </button>
+//   <button class="save-button" type="button">
+//     <i class="fa-regular fa-plus"></i>
+//     <p>save</p>
+//   </button>
+// </div>`;
+//   taskInput.value = "";
+//   // this.reset();
+
+//   list.appendChild(newTask);
+// }
--- Jade/JS/task.js
+++ Jade/JS/task.js
@@ -1,53 +1,65 @@
-const list = document.querySelector(".list");
+const list = document.querySelector(".task-list");
 
 // 必須用事件代理人，因為這樣動態新增的才可以吃到
 // 所以必須很多if
-list.addEventListener("click", clickHandler);
+// list.addEventListener("click", clickHandler);
 
-function clickHandler(e) {
-  let targetParent = e.target.closest(".task");
+// function clickHandler(e) {
+//   let targetParent = e.target.closest(".task");
 
-  // delete
-  if (
-    e.target.classList.contains("delete-button") ||
-    e.target.classList.contains("fa-x") ||
-    e.target.classList.contains("cancel-p")
-  ) {
-    list.removeChild(e.target.closest(".task"));
-    // e.target.parentElement.parentElement.remove();
-  }
+//   // delete
+//   if (
+//     e.target.classList.contains("delete-button") ||
+//     e.target.classList.contains("fa-x") ||
+//     e.target.classList.contains("cancel-p")
+//   ) {
+//     list.removeChild(e.target.closest(".task"));
+//     // e.target.parentElement.parentElement.remove();
+//   }
 
-  // save
-  if (e.target.classList.contains("save-button")) {
-    targetParent.children[1].classList.add("folded"); //task_body
-    targetParent.children[2].classList.add("folded"); //task_status
-    targetParent.children[0].classList.remove("task__head_border"); //task_head
-    targetParent.children[0].children[0].children[1].setAttribute(
-      "disabled",
-      "disabled"
-    );
-  }
+//   // save
+//   if (e.target.classList.contains("save-button")) {
+//     // 可以選到我要做事的那個，但無法指定他的父層是誰，所以都只能用到第一個
+//     // const taskBody = this.querySelector(".task__body");
+//     // taskBody.closest(".task").classList.add("folded");
+//     targetParent.children[1].classList.add("folded"); //task_body
+//     targetParent.children[2].classList.add("folded"); //task_status
+//     targetParent.children[0].classList.remove("task__head_border"); //task_head
+//     targetParent.children[0].children[0].children[1].setAttribute(
+//       "disabled",
+//       "disabled"
+//     );
+//   }
 
-  // edit
-  if (
-    e.target.classList.contains("edit-button") ||
-    e.target.classList.contains("fa-pen")
-  ) {
-    targetParent.children[1].classList.toggle("folded"); //task_body
-    targetParent.children[2].classList.toggle("folded"); //task_status
-    targetParent.children[0].classList.toggle("task__head_border"); //task_head
-    targetParent.children[0].children[0].children[1].removeAttribute(
-      "disabled"
-    );
-    //按壓edit關起來時不會增加input disable
-  }
+//   // edit
+//   if (
+//     e.target.classList.contains("edit-button") ||
+//     e.target.classList.contains("fa-pen")
+//   ) {
+//     targetParent.children[1].classList.toggle("folded"); //task_body
+//     targetParent.children[2].classList.toggle("folded"); //task_status
+//     targetParent.children[0].classList.toggle("task__head_border"); //task_head
+//     targetParent.children[0].children[0].children[1].removeAttribute(
+//       "disabled"
+//     );
+//     //按壓edit關起來時不會增加input disable
+//   }
 
-  // collect
-  if (
-    e.target.classList.contains("collect-button") ||
-    e.target.classList.contains("fa-star")
-  ) {
-    targetParent.classList.toggle("collect-mode"); //task
-    // targetParent.children[1].classList.toggle("collect-mode"); //task_body
-  }
-}
+//   // collect
+//   if (
+//     e.target.classList.contains("collect-button") ||
+//     e.target.classList.contains("fa-star")
+//   ) {
+//     targetParent.children[0].children[1].children[0].children[0].classList.toggle(
+//       "fa-regular"
+//     );
+//     targetParent.children[0].children[1].children[0].children[0].classList.toggle(
+//       "fa-solid"
+//     );
+//     targetParent.children[0].children[1].children[0].children[0].classList.toggle(
+//       "collect-color"
+//     );
+//     targetParent.classList.toggle("collect-mode"); //task
+//     // targetParent.children[1].classList.toggle("collect-mode"); //task_body
+//   }
+// }
