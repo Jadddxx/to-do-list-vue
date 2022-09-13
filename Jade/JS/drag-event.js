c2642d0533043fdaaa78d087e128038560e9316b
Jade <y3m45566@gmail.com>
2022-09-13 10:55:47 +0800

    drag, drop is donegit add .

--- Jade/JS/drag-event.js
+++ Jade/JS/drag-event.js
@@ -5,27 +5,27 @@ allTasks.forEach((task) => {
 });
 
 taskList.addEventListener("drop", dropHandler);
-taskList.addEventListener("dragover", dropoverHandler);
+taskList.addEventListener("dragover", dragoverHandler);
 taskList.addEventListener("dragenter", cancelDefault);
 
 let moveTarget;
 
 function dragHandler(e) {
-  console.log("drag start");
-  moveTarget = e.target;
-  console.log(moveTarget);
-  e.dataTransfer.setData("text/plain", e.target.dataset.uuid);
+  if (e.target.closest(".task").getAttribute("draggable")) {
+    console.log("drag start");
+    moveTarget = e.target.closest(".task");
+  } else {
+    console.log("not draggable target");
+  }
 }
 
 function dropHandler(e) {
-  cancelDefault(e);
-  const moveTargetUuid = e.dataTransfer.getData("text/plain");
-  moveTarget = document.querySelector(`.task[data-uuid="${moveTargetUuid}"]`);
+  const moveTargetUuid = moveTarget.dataset.uuid;
   const overItemUuid = overItem.dataset.uuid;
   let moveTargetIndex;
   let overItemIndex;
-  console.log(moveTargetUuid);
-  console.log(overItemUuid);
+  console.log(moveTarget.querySelector(".task__head__main").children[1].value);
+  console.log(overItem.querySelector(".task__head__main").children[1].value);
 
   tasks.forEach((task, index) => {
     if (task.uuid === Number(moveTargetUuid)) {
@@ -37,29 +37,37 @@ function dropHandler(e) {
   });
 
   if (overItem.classList.contains("borderAbove")) {
-    // -取代原本overItem的位置，再把原本的moveTarget刪掉
     taskList.insertBefore(moveTarget, overItem);
+
     // -moveTarget的index大於overItem的index
     if (moveTargetIndex > overItemIndex) {
       tasks.splice(overItemIndex, 0, tasks[moveTargetIndex]);
       tasks.splice(moveTargetIndex + 1, 1);
+
+      localStorage.setItem("tasks", JSON.stringify(tasks));
       // -moveTarget的index小於overItem的index
     } else if (moveTargetIndex < overItemIndex) {
       tasks.splice(overItemIndex, 0, tasks[moveTargetIndex]);
       tasks.splice(moveTargetIndex, 1);
-    }
 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    addTaskList(tasks, taskList);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
   }
   if (overItem.classList.contains("borderBelow")) {
-    // -取代overItem + 1的位置，再把原本的moveTarget刪掉
     taskList.insertBefore(moveTarget, overItem.nextElementSibling);
-    tasks.splice(overItemIndex, 0, tasks[moveTargetIndex]);
-    tasks.splice(moveTargetIndex, 1);
 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    addTaskList(tasks, taskList);
+    // -取代overItem + 1的位置，再把原本的moveTarget刪掉
+    if (moveTargetIndex < overItemIndex) {
+      tasks.splice(overItemIndex + 1, 0, tasks[moveTargetIndex]);
+      tasks.splice(moveTargetIndex, 1);
+
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    } else if (moveTargetIndex > overItemIndex) {
+      tasks.splice(overItemIndex + 1, 0, tasks[moveTargetIndex]);
+      tasks.splice(moveTargetIndex + 1, 1);
+
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
   }
 
   cancelOverItem(e);
@@ -67,8 +75,8 @@ function dropHandler(e) {
 
 let overItem = null;
 
-function dropoverHandler(e) {
-  cancelDefault(e);
+function dragoverHandler(e) {
+  e.preventDefault();
   cancelOverItem(e);
 
   // !如果有經過task之間的空格是task-list要避掉
