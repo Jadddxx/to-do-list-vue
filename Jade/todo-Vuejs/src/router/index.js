f4d4ee27bbe87c9a482a48f501dd0c31c25cb60a
Jade <y3m45566@gmail.com>
2022-10-20 21:29:45 +0800

    modify path

--- Jade/todo-Vuejs/src/router/index.js
+++ Jade/todo-Vuejs/src/router/index.js
@@ -4,20 +4,20 @@ import Complete from "@/views/Complete.vue";
 import InProgress from "@/views/InProgress.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("/to-do-list-vue/Jade/todo-Vuejs/dist"),
   routes: [
     {
-      path: "/to-do-list-vue/Jade/todo-Vuejs/dist/",
+      path: "/",
       name: "MyTasks",
       component: MyTasks,
     },
     {
-      path: "/to-do-list-vue/Jade/todo-Vuejs/dist/complete",
+      path: "/complete",
       name: "Complete",
       component: Complete,
     },
     {
-      path: "/to-do-list-vue/Jade/todo-Vuejs/dist/inProgress",
+      path: "/inProgress",
       name: "InProgress",
       component: InProgress,
     },
