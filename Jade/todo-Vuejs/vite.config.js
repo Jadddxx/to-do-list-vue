6f20dd6b7918d33cbb9c9f2cbdb9118727fa5fb4
Jade <y3m45566@gmail.com>
2022-10-20 18:49:07 +0800

    modify base for github repo path

--- Jade/todo-Vuejs/vite.config.js
+++ Jade/todo-Vuejs/vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Jadddxx/to-do-list-vue/settings/pages/",
   plugins: [vue()],
   resolve: {
     alias: {
