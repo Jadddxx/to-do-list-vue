e2c5e7a0caa575f5b02a3ce40f7540b81163d7dc
Jade <y3m45566@gmail.com>
2022-10-20 20:21:45 +0800

    modify path

--- Jade/todo-Vuejs/src/router/index.js
+++ Jade/todo-Vuejs/src/router/index.js
@@ -3,8 +3,9 @@ import MyTasks from "@/views/MyTasks.vue";
 import Complete from "@/views/Complete.vue";
 import InProgress from "@/views/InProgress.vue";
 
+const base = "/to-do-list-vue/Jade/todo-Vuejs/dist/";
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(base),
   routes: [
     {
       path: "/",
--- Jade/todo-Vuejs/vite.config.js
+++ Jade/todo-Vuejs/vite.config.js
@@ -5,7 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/to-do-list-vue/Jade/todo-Vuejs/dist/",
   plugins: [vue()],
   resolve: {
     alias: {
