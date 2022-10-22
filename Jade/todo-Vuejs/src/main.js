18e497b7bd95a02d440c8fcab5b17a4d0e19096c
Jade <y3m45566@gmail.com>
2022-10-22 16:09:30 +0800

    modify date input flex to column in mobile.

--- Jade/todo-Vuejs/src/main.js
+++ Jade/todo-Vuejs/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./assets/style.css";
+// import "./assets/style.css";
+import "./assets/scss/style.scss";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import "@/font-awesome/index.js";
 import router from "@/router/index.js";
